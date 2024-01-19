253c4af86b1a2a1f51c5f3391b5c9d47e5b97aea
federicomanni97 <manni_federico@libero.it>
2024-01-19 15:25:20 +0100

    initialization scaffolding vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

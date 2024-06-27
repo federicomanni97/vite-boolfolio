998ba8d5368f5bff0bc9fdefe9b0f5f1876cb60d
federicomanni97 <manni_federico@libero.it>
2024-06-27 21:51:06 +0200

    portfolio style fixes

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="d-flex align-items-center">
+        <div class="w-25">
+            <img class="my-image" src="../assets/images/Black-bg-photo.jpg" alt="">
+        </div>
+        <div>
+            <h1 class="text-danger">About Me:</h1>
+            <p class="text-light fs-3">Ciao sono Federico Manni, e mi considero una persona socievole e comunicativa, con una naturale propensione al lavoro di squadra. Sono appassionato di tecnologia e mi entusiasma l'idea di contribuire a progetti innovativi, mentre continuo a cercare nuove opportunità per crescere sia professionalmente che personalmente.</p>
+        </div>
+    </div>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h2 class="text-light">Le mie competente:</h2>
+        <div class="d-flex align-items-center justify-content-center m-2">
+            <h4>HTML</h4>
+            <h4>CSS</h4>
+            <h4>Bootstrap</h4>
+            <h4>Javascript</h4>
+            <h4>SCSS</h4>
+            <h4>VueJS</h4>
+            <h4>Laravel</h4>
+            <h4>PHP</h4>
+        </div>
     </div>
 </template>
 

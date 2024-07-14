2bf4fc627576c422aa0a3d6086bab761084b5854
federicomanni97 <manni_federico@libero.it>
2024-07-14 15:26:03 +0200

    portfolio-fixes

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -4,25 +4,21 @@
             <div class="mb-3">
                 <label for="name" class="form-label">Your Name</label>
                 <input type="text" class="form-control" id="email" aria-describedby="nameHelp" v-model="name">
-                <div id="nameHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="address" class="form-label">Your Address</label>
                 <input type="text" class="form-control" id="email" aria-describedby="addressHelp" v-model="address">
-                <div id="addressHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Your message</label>
                 <textarea type="text" class="form-control" id="email" aria-describedby="message" v-model="message"></textarea>
-                <div id="messageHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button type="reset" class="btn btn-info me-2">Reset</button>
+            <button type="submit" class="btn btn-primary my-3 p-2">Submit</button>
+            <button type="reset" class="btn btn-danger my-3 p-2">Reset</button>
         </form>
     </div>
 </template>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-flex align-items-center">
+    <div class="d-flex align-items-center mt-3">
         <div class="w-25">
             <img class="my-image" src="../assets/images/Black-bg-photo.jpg" alt="">
         </div>
@@ -9,17 +9,42 @@
         </div>
     </div>
     <div class="d-flex flex-column align-items-center justify-content-center">
-        <h2 class="text-light">Le mie competente:</h2>
-        <div class="d-flex align-items-center justify-content-center m-2">
-            <h4>HTML</h4>
-            <h4>CSS</h4>
-            <h4>Bootstrap</h4>
-            <h4>Javascript</h4>
-            <h4>SCSS</h4>
-            <h4>VueJS</h4>
-            <h4>Laravel</h4>
-            <h4>PHP</h4>
+        <h2 class="text-danger">Le mie competenze:</h2>
+        <div class="text-light d-flex align-items-center justify-content-center m-2">
+            <div>
+                <h4>HTML</h4>
+            </div>
+            <div>
+                <h4>CSS</h4>
+            </div>
+            <div>
+                <h4>Bootstrap</h4>
+            </div>
+            <div>
+                <h4>Javascript</h4>
+            </div>
+            <div>
+                <h4>SCSS</h4>
+            </div>
+            <div>
+                <h4>VueJS</h4>
+            </div>
+            <div>
+                <h4>Laravel</h4>
+            </div>
+            <div>
+                <h4>PHP</h4>
+            </div>
         </div>
+    </div>
+    <div>
+        <h2 class="text-light">
+            Contacts:
+        </h2>
+        <p class="text-light fs-5">+39 345 92 32 030</p>
+        <p class="text-light fs-5">Email: manni_federico@libero.it</p>
+        <p class="text-light fs-5">Github: github.com/federicomanni97</p>
+        <p class="text-light fs-5">Linkedin: linkedin.com/in/federicomanni2909/</p>
     </div>
 </template>
 
